1b35d6c5c4a58af69b3ac42e0fbf5153d75ffe61
Pavel <tukmachev.pavel.ru@yandex.ru>
2018-04-09 11:28:33 +0700

    update

--- target_indicator/js/script.js
+++ target_indicator/js/script.js
@@ -1,6 +1,6 @@
 jQuery('document').ready(function () {
     jQuery.ajax({
-        url: 'https://paveltuk.github.io/paveltuk.github.io/target_indicator_api/'
+        url: 'http://alex.devel.softservice.org/testapi/'
         , method: 'get'
         , dataType: 'json'
         , success: function (data) {
